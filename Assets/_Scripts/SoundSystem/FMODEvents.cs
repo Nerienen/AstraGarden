d2f3bd22ed84572ed4eaaf131be8438b5cf9f505
CarlosQui2001 <carlosqui2001@gmail.com>
2023-07-19 21:50:54 +0200

    SOUND BRANCH FFS

--- Assets/_Scripts/SoundSystem/FMODEvents.cs
+++ Assets/_Scripts/SoundSystem/FMODEvents.cs
@@ -5,19 +5,53 @@ using UnityEngine;
 
 public class FMODEvents : MonoBehaviour
 {
+   
     [field: Header("Ambience")]
-    //[field: SerializeReference] public EventReference ambience { get; private set; }
-    
+    [field: SerializeField] public EventReference lowHum { get; private set; }
+    [field: SerializeField] public EventReference fan { get; private set; }
+    [field: SerializeField] public EventReference rattlingAC { get; private set; }
+
+
     [field: Header("Music")]
     [field: SerializeField] public EventReference drift { get; private set; }
     [field: SerializeField] public EventReference ending { get; private set; }
-    
+
+
+    #region Gun and blob sounds
+
+    [field: Header("Gun SFX")]
+    [field: SerializeField] public EventReference chargeWater { get; private set; }
+    [field: SerializeField] public EventReference chargeEnergy{ get; private set; }
+    [field: SerializeField] public EventReference shootWater { get; private set; }
+    [field: SerializeField] public EventReference shootEnergy { get; private set; }
+
+    [field: SerializeField] public EventReference changeMode { get; private set; }
+
+
     [field: Header("Water SFX")]
-    //[field: SerializeReference] public EventReference inflationSound { get; private set; }
-    //[field: SerializeReference] public EventReference blobSound { get; private set; }
-    
+    [field: SerializeField] public EventReference waterReboundOnWall { get; private set; }
+    [field: SerializeField] public EventReference waterDisipate { get; private set; }
+    [field: SerializeField] public EventReference waterFloatingSound { get; private set; }
+
+
+    [field: Header("Water SFX")]
+    [field: SerializeField] public EventReference energyReboundOnWall { get; private set; }
+    [field: SerializeField] public EventReference energyDisipate { get; private set; }
+    [field: SerializeField] public EventReference energyFloatingSound { get; private set; }
+
+    #endregion
+
+
+
+    #region Misc Object Sounds
+
+
+    #endregion
+
     [field: Header("Player SFX")]
-    //[field: SerializeReference] public EventReference footStepsSound { get; private set; }
+    [field: SerializeField] public EventReference footSteps { get; private set; }
+
+    
 
     
     
--- Assets/_Scripts/SoundSystem/MusicManager.cs
+++ Assets/_Scripts/SoundSystem/MusicManager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class MusicManager : MonoBehaviour
 {
+
+
+    #region Game State Variables
+    public bool isIntro = true;
+    public bool hasTriggeredIntro = false;
+
+    public bool isNormal = false;
+
+    public bool isBlackout = false;
+    public bool hasTriggeredBlackout = false;
+
+    public bool isEnding = false;
+    public bool hasTriggeredTransitionToEnd = false;
+    public bool hasTriggeredEendingSequence = false;
+    #endregion
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Get game state
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Get game state
         
+
+
+
     }
+
+
 }
